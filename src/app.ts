3f96f31d029b42626607bca8a5f3b69c3c0b5c6b
CristiAndLook <krhistian1902@gmail.com>
2024-04-27 00:03:52 -0500

    WWeb Server with Express

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
-
+import { Server } from './presentation/server'
+import { envs } from './config/envs'
 
 (async () => {
     main()
 })()
 
 async function main() {
-    console.log('Hello World')
+    const server = new Server({
+        port: envs.PORT,
+        public_path: envs.PUBLIC_PATH,
+    })
+    server.start()
 }
