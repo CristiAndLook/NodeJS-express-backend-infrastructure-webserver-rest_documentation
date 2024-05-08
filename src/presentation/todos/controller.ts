ca2cabf01378c5fbbec6c4b2dea1f6e79c3dae7d
CristiAndLook <krhistian1902@gmail.com>
2024-05-07 23:11:58 -0500

    Refactoring with clean architecture, repositories, entities, datasources

--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -1,100 +1,62 @@
 import { Request, Response } from 'express';
 import { prisma } from '../../data/postgres';
-import { CreateTodoDto, UpdateTodoDto } from '../../domain/dtos/index';
+import { CreateTodoDto, UpdateTodoDto } from '../../domain/dtos';
+import { TodoRepository } from '../../domain';
+
 
 export class TodosController {
+
     //* DI
-    constructor() { }
+    constructor(
+        private readonly todoRepository: TodoRepository,
+    ) { }
+
 
     public getTodos = async (req: Request, res: Response) => {
-
-        await prisma.todo.findMany()
-            .then(todos => {
-                res.json(todos);
-            }).catch(err => {
-                res.status(500).json({ error: err.message });
-            });
-
-    }
+        const todos = await this.todoRepository.getAll();
+        return res.json(todos);
+    };
 
     public getTodoById = async (req: Request, res: Response) => {
         const id = +req.params.id;
 
-        if (isNaN(id)) return res.status(400).json({ error: 'ID argument is not a number' });
-
-        await prisma.todo.findUnique({
-            where: {
-                id: id
-            }
-        }).then(todo => {
-            if (!todo) return res.status(404).json({ error: `Todo with id ${id} not found` });
+        try {
+            const todo = await this.todoRepository.findById(id);
             res.json(todo);
-        }).catch(err => {
-            res.status(500).json({ error: err.message });
-        });
-    }
+
+        } catch (error) {
+            res.status(400).json({ error });
+        }
+
+    };
 
     public createTodo = async (req: Request, res: Response) => {
         const [error, createTodoDto] = CreateTodoDto.create(req.body);
-        
         if (error) return res.status(400).json({ error });
 
-        await prisma.todo.create({
-            data: createTodoDto!
-        }).then(todo => {
-            res.json(todo);
-        }).catch(err => {
-            res.status(500).json({ error: err.message });
-        });
+        const todo = await this.todoRepository.create(createTodoDto!);
+        res.json(todo);
+
     };
 
     public updateTodo = async (req: Request, res: Response) => {
         const id = +req.params.id;
         const [error, updateTodoDto] = UpdateTodoDto.create({ ...req.body, id });
-
         if (error) return res.status(400).json({ error });
 
+        const updatedTodo = await this.todoRepository.updateById(updateTodoDto!);
+        return res.json(updatedTodo);
 
-        await prisma.todo.findUnique({
-            where: {
-                id: id
-            }
-        }).then(todo => {
-            if (!todo) return res.status(404).json({ error: `Todo with id ${id} not found` });
-
-            const { text, completedAt } = req.body;
-
-            prisma.todo.update({
-                where: {
-                    id: id
-                },
-                data: updateTodoDto!.values
-            }).then(todo => {
-                res.json(todo);
-            }).catch(err => {
-                res.status(500).json({ error: err.message });
-            });
-
-        }).catch(err => {
-            res.status(500).json({ error: err.message });
-        });
-
-    }
+    };
 
 
     public deleteTodo = async (req: Request, res: Response) => {
         const id = +req.params.id;
-        if (isNaN(id)) return res.status(400).json({ error: 'ID argument is not a number' });
+        const deletedTodo = await this.todoRepository.deleteById(id);
+        res.json(deletedTodo);
 
-        await prisma.todo.delete({
-            where: {
-                id: id
-            }
-        }).then(todo => {
-            if (!todo) return res.status(404).json({ error: `Todo with id ${id} not found` });
-            res.json(todo);
-        }).catch(err => {
-            res.status(500).json({ error: err.message });
-        });
-    }
+    };
+
+
+
 }
